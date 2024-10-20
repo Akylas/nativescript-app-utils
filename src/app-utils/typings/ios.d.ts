06c5e36aec45381597981a99203605f6cc0439d3
Martin Guillon <dev@akylas.fr>
2024-10-20 09:39:37 +0200

    fix: package native typings

--- src/app-utils/typings/ios.d.ts
+++ src/app-utils/typings/ios.d.ts
@@ -13,13 +13,15 @@ declare class ImageUtils extends NSObject {
 
     static getAspectSafeDimensions(sourceWidth: number, sourceHeight: number, reqWidth: number, reqHeight: number): CGSize;
 
+    static getImageSize(src: string): NSDictionary<string, any>;
+
     static new(): ImageUtils; // inherited from NSObject
 
-    static readImageFromFileOptions(src: string, options: NSDictionary<any, any>): UIImage;
+    static readImageFromFile(src: string, delegate: NCompletionDelegate, stringOptions: string): void;
 
-    static readImageFromFileSync(src: string, stringOptions?: string): UIImage;
-    static readImageFromFile(src: string, delegate: NCompletionDelegate, stringOptions?: string);
-    static getImageSize(src: string): NSDictionary<string, any>;
+    static readImageFromFileSync(src: string, stringOptions: string): UIImage;
+
+    static readImageFromFileSyncOptions(src: string, options: NSDictionary<any, any>): UIImage;
 
     static scaleImage(image: UIImage, scaledImageSize: CGSize): UIImage;
 
